f88f811d16bab561d4d25307ac97069d67e0f459
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-01-24 22:51:09 +0200

    Сделал 2 задачу

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,11 +9,21 @@ galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
 function createImageGalleryMarkup(gallery) {
   return gallery
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
-            </a>
-        </div>`;
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
     })
     .join("");
 }
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
