4a09167e2ca7c3a7e9a062943eb4e5d9715fcf6d
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-01-24 14:46:22 +0200

    Сделал 1 задачу

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createImageGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createImageGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+            </a>
+        </div>`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+  const isGalleryImageEl = event.target.classList.contains("gallery__image");
+
+  if (!isGalleryImageEl) {
+    return;
+  }
+
+  const urlImage = event.target.dataset.source;
+  const altModalImage = event.target.alt;
+
+  let instance = basicLightbox.create(
+    `<img src="${urlImage}" alt="${altModalImage}">`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+}
